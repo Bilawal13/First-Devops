eced3fa271f3cfe911e75aa87ae6f27c58800997
Bilawal Feroze <155391296+Bilawal13@users.noreply.github.com>
2026-03-17 11:36:03 +0500

    Delete new_shopping_list directory

--- new_shopping_list/android/app/src/main/kotlin/com/example/new_shopping_list/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.new_shopping_list
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- new_shopping_list/lib/data/dummy.dart
+++
@@ -1,8 +0,0 @@
-import 'package:new_shopping_list/data/categories.dart';
-import 'package:new_shopping_list/models/category.dart';
-import 'package:new_shopping_list/models/grocery_list.dart';
-
-final dummy_data = [
-  GroceryList('a', 'chocolate', 12, categories[Categories.sweets]!),
-  GroceryList('b', 'spice', 12, categories[Categories.spices]!),
-];
